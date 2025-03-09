a229686addd77e0e67d29229ead936ca6c6377c6
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2025-03-09 18:30:05 +0300

    Update extraction.py

--- gitaristmusic/utils/extraction.py
+++ gitaristmusic/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from gitaristusic import app
+from gitaristmusic import app
 
 
 async def extract_user(m: Message) -> User:
