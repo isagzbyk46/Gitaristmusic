7eb5cae14c9398602799d681739c56f719b3fb40
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2025-03-09 18:24:21 +0300

    Update admins.py

--- gitaristmusic/utils/decorators/admins.py
+++ gitaristmusic/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from gitaristusic import app
-from gitaristusic.misc import SUDOERS, db
-from gitaristusic.utils.database import (
+from gitaristmusic import app
+from gitaristmusic.misc import SUDOERS, db
+from gitaristmusic.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
