5460940030d8214a6127d1f244ad79173271523b
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2025-03-09 17:25:03 +0300

    Update help.py

--- gitaristmusic/plugins/bot/help.py
+++ gitaristmusic/plugins/bot/help.py
@@ -3,11 +3,11 @@ from typing import Union
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from TGNMusic import app
-from TGNMusic.utils import help_pannel
-from TGNMusic.utils.database import get_lang
-from TGNMusic.utils.decorators.language import LanguageStart, languageCB
-from TGNMusic.utils.inline.help import help_back_markup, private_help_panel
+from gitaristmusic import app
+from gitaristmusic.utils import help_pannel
+from gitaristmusic.utils.database import get_lang
+from gitaristmusic.utils.decorators.language import LanguageStart, languageCB
+from gitaristmusic.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
 
