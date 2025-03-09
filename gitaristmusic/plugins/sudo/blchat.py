6d45cf588be4ff1b052937c4a4e3f4fedb6d3a23
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2025-03-09 17:35:38 +0300

    Update blchat.py

--- gitaristmusic/plugins/sudo/blchat.py
+++ gitaristmusic/plugins/sudo/blchat.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from TGNMusic import app
-from TGNMusic.misc import SUDOERS
-from TGNMusic.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
-from TGNMusic.utils.decorators.language import language
+from gitaristmusic import app
+from gitaristmusic.misc import SUDOERS
+from gitaristmusic.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
+from gitaristmusic.utils.decorators.language import language
 from config import BANNED_USERS
 
 
