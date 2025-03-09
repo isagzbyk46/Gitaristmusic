55b581320b4b2f36f56da33f9229322de65ac6eb
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2025-03-09 17:56:06 +0300

    Update logger.py

--- gitaristmusic/utils/logger.py
+++ gitaristmusic/utils/logger.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import ParseMode
 
-from TGNMusic import app
-from TGNMusic.utils.database import is_on_off
+from gitaristusic import app
+from gitaristusic.utils.database import is_on_off
 from config import LOG_GROUP_ID
 
 
