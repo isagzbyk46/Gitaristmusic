afdde81ad9af99cd79e62789bb9cc9783e4cca10
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2025-03-09 18:26:02 +0300

    Update help.py

--- gitaristmusic/utils/inline/help.py
+++ gitaristmusic/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from gitaristusic import app
+from gitaristmusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
