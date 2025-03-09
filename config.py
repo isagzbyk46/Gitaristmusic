0570848bcef9aef67776c50597fb72219da18eab
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2025-03-09 20:22:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -114,7 +114,7 @@ if SUPPORT_CHANNEL:
         )
 
 if SUPPORT_CHAT:
-    if not re.match("https://t.me/gitaristmusic12"):
+    if not re.match("(?:http|https)://", "https://t.me/gitaristmusic12"):
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
         )
