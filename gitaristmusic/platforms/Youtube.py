d585120c539fd71b331d704feb75420aa8522d23
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2025-03-09 17:08:12 +0300

    Update Youtube.py

--- gitaristmusic/platforms/Youtube.py
+++ gitaristmusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from TGNMusic.utils.database import is_on_off
-from TGNMusic.utils.formatters import time_to_seconds
+from gitaristmusic.utils.database import is_on_off
+from gitaristmusic.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
