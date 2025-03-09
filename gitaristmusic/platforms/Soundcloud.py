2b5d6e9f4d0114e9b7291b6208f955314846feb3
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2025-03-09 17:04:26 +0300

    Update Soundcloud.py

--- gitaristmusic/platforms/Soundcloud.py
+++ gitaristmusic/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from TGNMusic.utils.formatters import seconds_to_min
+from gitaristmusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
