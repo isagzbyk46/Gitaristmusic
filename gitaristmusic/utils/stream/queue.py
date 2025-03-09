9001e4b59407631b36323cd4c9c2b61120ba7ce4
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2025-03-09 18:27:40 +0300

    Update queue.py

--- gitaristmusic/utils/stream/queue.py
+++ gitaristmusic/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from gitaristusic.misc import db
-from gitaristusic.utils.formatters import check_duration, seconds_to_min
+from gitaristmusic.misc import db
+from gitaristmusic.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
